27ffa0b25ba7560e080ef3230c0ec7209f63f39d
Fortune Smiles <worldsbestbrent@gmail.com>
2021-09-04 19:05:35 -0400

    set up user location fetch

--- assets/js/location.js
+++ assets/js/location.js
@@ -1,17 +1,20 @@
 
-async function loadData() {
+async function fetchLocation() {
    return await (await fetch('http://api.ipstack.com/check?access_key=83efe3074e441fc99a26233154d65f0e')).json();
 }
 
-async function displayData() {
+async function getUserLocation() {
    try {
-      data = await loadData();
-      console.log(data);
-
+      response = await fetchLocation();
+      console.log(`After fetch: ${response}`);
+      // Get state initials from user location
+      userLocation = response.region_code;
+      // Pass the user location to filter artist events with
+      fetchData(userLocation);
    }
    catch (err) {
       alert(`Uh oh, something went wrong: ${err}!`);
    }
 };
 
-displayData();
+getUserLocation();
