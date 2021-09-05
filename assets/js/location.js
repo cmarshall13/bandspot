de15ac79cfa00a7fd61732242e6654a9c29db6ea
Fortune Smiles <worldsbestbrent@gmail.com>
2021-09-05 14:03:20 -0400

    made userLocation variable global and removed function passing for cleaner code

--- assets/js/location.js
+++ assets/js/location.js
@@ -1,3 +1,4 @@
+let userLocation = '';
 
 async function fetchLocation() {
    return await (await fetch('http://api.ipstack.com/check?access_key=83efe3074e441fc99a26233154d65f0e')).json();
@@ -8,9 +9,7 @@ async function getUserLocation() {
       response = await fetchLocation();
       console.log(`After fetch: ${response}`);
       // Get state initials from user location
-      let userLocation = response.region_code;
-      // Pass the user location to filter artist events with
-      artistLoopForFetch(userLocation);
+      userLocation = response.region_code;
    }
    catch (err) {
       alert(`Uh oh, something went wrong: ${err}!`);
