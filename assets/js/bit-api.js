1f76d1d12374908f73e12e14973f18c75cdf67f2
Fortune Smiles <worldsbestbrent@gmail.com>
2021-09-08 10:01:36 -0400

    completed functionality for new location api

--- assets/js/bit-api.js
+++ assets/js/bit-api.js
@@ -2,29 +2,29 @@ var eventsArray = [];
 var FETCH_URL = '';
 const API_KEY = '641fbf4191ad9a6cd474d32af802738c';
 
-var fetchConcertData = async function(artists){
-   for(var artist of artists){
+var fetchConcertData = async function (artists) {
+   for (var artist of artists) {
       FETCH_URL = `https://rest.bandsintown.com/artists/${artist}/events/?app_id=${API_KEY}`;
-      try{
+      try {
          await fetch(FETCH_URL)
-         .then(function(response){
-            return response.json();
-         })
-         .then(function(data){
-            saveConcertData(artist, data, eventsArray.length);
-         });
-      }catch(e){
-         console.log(e);
+            .then(function (response) {
+               return response.json();
+            })
+            .then(function (data) {
+               saveConcertData(artist, data, eventsArray.length);
+            });
+      } catch (e) {
+         displayErrorModal();
          continue;
       }
    }
    filterLocation();
 }
 
-var saveConcertData = function(artist, data, index){
-   eventsArray.push({artist: artist, shows: []});
+var saveConcertData = function (artist, data, index) {
+   eventsArray.push({ artist: artist, shows: [] });
 
-   for(var d of data){
-      eventsArray[index].shows.push({date: d.datetime, venue: d.venue.name, location: d.venue.location, region: d.venue.region});
+   for (var d of data) {
+      eventsArray[index].shows.push({ date: d.datetime, venue: d.venue.name, location: d.venue.location, region: d.venue.region });
    }
 }
