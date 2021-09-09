ff796633ddfa452256af57d534d9b9f8ebeede6b
Fortune Smiles <worldsbestbrent@gmail.com>
2021-09-09 17:23:48 -0400

    changed redirect url to live server

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ var hideLoginDiv = function () {
 
 
 var populateRegionSelectBox = function () {
-    DOMEl.regionColumnEl.removeAttribute("class", "hidden");
+
     var opt = document.createElement("option");
     opt.value = userLocation;
     opt.textContent = userLocation;
@@ -28,7 +28,7 @@ var populateRegionSelectBox = function () {
 
     for (var e of eventsArray) {
         // check if that event object has any shows
-        if(e.shows.length > 0){
+        if (e.shows.length > 0) {
             var region = e.shows[0].region;
             // If the current region does not match any of the options
             if (!DOMEl.regionSelect.innerHTML.includes(region)) {
@@ -58,6 +58,7 @@ var populateArtistSelectBox = function () {
 
     DOMEl.artColumnEl.removeAttribute("class", "hidden");
     DOMEl.monthColumnEl.removeAttribute("class", "hidden");
+    DOMEl.regionColumnEl.removeAttribute("class", "hidden");
     DOMEl.optionsDiv.removeAttribute("class", "hidden");
 }
 
@@ -143,16 +144,16 @@ var filter = function (artistFilter, monthFilter, regionFilter) {
                 // check if all filters are "All"
                 if (artistFilter === "All" && monthFilter === "All" && regionFilter === "All") {
                     displayConcertCards(artist, image, show);
-                // check if artist & region are "All"
+                    // check if artist & region are "All"
                 } else if (artistFilter === "All" && month === monthFilter && regionFilter === "All") {
                     displayConcertCards(artist, image, show);
-                // check if month & region are "All"
+                    // check if month & region are "All"
                 } else if (artist === artistFilter && monthFilter === "All" && regionFilter === "All") {
                     displayConcertCards(artist, image, show);
-                // check if artist & month are "All"
+                    // check if artist & month are "All"
                 } else if (artistFilter === "All" && monthFilter === "All" && region === regionFilter) {
                     displayConcertCards(artist, image, show);
-                // check if all are variable
+                    // check if all are variable
                 } else if (artist === artistFilter && month === monthFilter && region === regionFilter) {
                     displayConcertCards(artist, image, show);
                 }
